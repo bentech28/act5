b8fa099c9124140e2feba6e0ef2e0fd3778d3089
Bounyamine Baparapé <baparape@gmail.com>
2025-11-08 06:36:32 +0100

    ACT5

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// lib/main.dart
+
+import 'package:flutter/material.dart';
+import 'redacteurs_interface.dart';
+
+void main() {
+  runApp(const MonApplication());
+}
+
+class MonApplication extends StatelessWidget {
+  const MonApplication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gestion des Rédacteurs',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const RedacteursInterface(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
